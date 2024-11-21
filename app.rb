36bd9dc729de0e25cb351142cc50470e52e82d7b
Guilherme Rocha <jguilherme.1997@gmail.com>
2024-11-21 16:04:45 -0300

    Criação do menu e escolhas iniciais

--- app.rb
+++ app.rb
@@ -1,15 +1,39 @@
+require_relative 'models/menu'
+
 class App
+  attr_accessor :continua_executar
 
   def initialize
-    
+    @continua_executar = true
+    @menu = Menu.new
   end
 
-  def teste_inicio
-    puts 'Teste execução'
+  def inicio
+    executa
   end
 
   private
 
+  def executa
+    while continua_executar
+      exibe_menu
+      continua_executar?
+    end
+  end
+
+  def exibe_menu
+    @menu.listagem_menu
+  end
+
+  def continua_executar?
+    escolha_usuario = gets.chomp.to_i
+    escolhas = { 0 => :exit, 1 => :lista_task }
+    
+    if escolhas.include?(escolha_usuario) && escolhas[escolha_usuario] == :exit
+      self.continua_executar = false
+    end
+  end
+
 end
 
-App.new.teste_inicio
+App.new.inicio
---
+++ models/menu.rb
@@ -0,0 +1,22 @@
+class Menu
+  attr_reader :escolhas
+
+  def initialize
+    @escolhas = {
+      0 => :exit,
+      1 => :lista_task
+    }
+
+    @descricoes = {
+      exit: "Sair",
+      lista_task: "Listar Tarefas"
+    }
+  end
+
+  def listagem_menu
+    @escolhas.each do |key, value|
+      descricao = @descricoes[value] || value.to_s
+      puts "#{key.to_s.center(5, ' ')} -> #{descricao}"
+    end
+  end
+end
